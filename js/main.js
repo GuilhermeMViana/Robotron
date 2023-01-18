11f634a6a009b93dedb0d11a368e318c7a608dee
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-01-18 10:01:35 -0300

    Manipulando dados via JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
-const robotron = document.querySelector('.robo')
+const subtrair = document.querySelector('#subtrair')
+const somar = document.querySelector('#somar')
+const braco = document.querySelector('#braco')
 
-robotron.addEventListener("click", (event) => {
-    console.log(event);
-});
+const controle = document.querySelectorAll(".controle-ajuste")
+const pecas = document.querySelectorAll(".controle-contador")
 
-function conversaRobotron(nome) {
-    console.log(`Olá, ${nome}`);
+controle.forEach((element) => {
+    element.addEventListener("click", (event) => { manipuladorDeDados(event.target.textContent) })
+})
+
+function manipuladorDeDados(operacao) {
+
+    for (let i = 0; i < pecas.length; i++) {
+        if (operacao === "-") {
+            if (pecas[i].value > 0) {
+                pecas[i].value = parseInt(braco.value) - 1
+            }
+        } else {
+            pecas[i].value = parseInt(braco.value) + 1
+        }
+    }
 }
-
-conversaRobotron("Guilherme")
