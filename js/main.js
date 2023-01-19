a2b8900c03c0580031103daa6a0440964e079fd5
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-01-19 10:00:38 -0300

    Adicionando funcionalidade de calcular o poder

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
+const estatistica = document.querySelectorAll("[data-estatistica]")
 const controle = document.querySelectorAll("[data-controle]")
+const partes = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach((element) => {
     element.addEventListener("click", (event) => {
         manipuladorDeDados(event.target.dataset.controle, event.target.parentNode)
+        atualizarEstatiticas(event.target.dataset.pecas)
     })
 })
 
@@ -15,5 +50,12 @@ function manipuladorDeDados(operacao, controle) {
     } else {
         pecas.value = parseInt(pecas.value) + 1
     }
+}
+
+function atualizarEstatiticas(pecas) {
+    console.log(partes[pecas])
+    estatistica.forEach((element) => {
+        element.textContent = parseInt(element.textContent) + partes[pecas][element.dataset.estatistica]
+    })
 }
 
